08f7ac33d02386b32e1566dae7280b596a625893
rinathiLM <rina.thi@libertymutual.com>
2017-10-23 21:19:08 -0400

    CSS fix
    CSS fix to opening page

--- assets/scripts/hikes/ui.js
+++ assets/scripts/hikes/ui.js
@@ -1,10 +1,13 @@
 'use strict'
 const showHikesTemplate = require('../templates/hike-listing.handlebars')
+// const api = require('./api')
+// const ui = require('./ui')
 
 const createHikeSuccess = (data) => {
   console.log('Successfully created hike ', data)
-  $('#create-hike-modal').modal('hide')
-  $('#create-hike-modal').empty()
+  $('#create-hike-modal-title').text('Yay, you just inputted a new adventure! Close this box to see your recent input.')
+  // $('#create-hike-modal').modal('hide')
+  // $('#create-hike-modal').empty()
   $('#hike-messages').text('Successfully added hike! Click the "See my hikes" button to see all your entries.')
 }
 
@@ -37,6 +40,13 @@ const deleteHikeFailure = () => {
 
 const updateHikeSuccess = (data) => {
   console.log('Successful update')
+  // add this to UI to see if it adds it autmatoically
+  // api.getHikes()
+  //   .then(ui.getHikesSuccess)
+  //   // had to do the below so that all of these run before my handlebars since the button functionality relies on the below
+  //   .then(() => $('.update-hike-form').on('submit', onUpdateHike))
+  //   .then(() => $('.delete-hike-link').on('click', onDeleteHike))
+    // .catch(ui.getHikesFailure)
   $('.update-hike-modal-title').text('Your inputs have been updated!')
   $('.update-modal-body').hide()
 }
